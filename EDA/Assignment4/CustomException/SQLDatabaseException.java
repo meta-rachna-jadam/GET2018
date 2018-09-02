5e7eb6a2f90cbaff6b50439c80dba82728330b30
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-02 23:21:05 +0530

    Update SQLDatabaseException.java

--- EDA/Assignment4/CustomException/SQLDatabaseException.java
+++ EDA/Assignment4/CustomException/SQLDatabaseException.java
@@ -1,5 +1,10 @@
 package com.CustomException;
 
-public class SQLDatabaseException {
+import com.model.DatabaseException;
 
+public class SQLDatabaseException extends Exception{
+
+	public SQLDatabaseException(String className, String exceptionNam, String exceptionDescription) {
+	    new DatabaseException(className, exceptionNam, exceptionDescription);
+	}
 }
