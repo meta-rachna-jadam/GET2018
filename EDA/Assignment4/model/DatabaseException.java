6a8fe606f7378988b92bf8cfea3ce77a02e1e281
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-02 23:26:10 +0530

    Update DatabaseException.java

--- EDA/Assignment4/model/DatabaseException.java
+++ EDA/Assignment4/model/DatabaseException.java
@@ -1,15 +1,42 @@
 package com.model;
 
+import com.controller.ExceptionController;
+
 public class DatabaseException {
     private String className;
-    private String exceptionNam;
+    private String exceptionName;
     private String exceptionDescription;
     
-	public DatabaseException(String className, String exceptionNam,
+	public DatabaseException(String className, String exceptionName,
 			String exceptionDescription) {
 		
+		this.setClassName(className);
+		this.setExceptionName(exceptionName);
+		this.setExceptionDescription(exceptionDescription);
+		new ExceptionController().createExeption(this);
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
 		this.className = className;
-		this.exceptionNam = exceptionNam;
+	}
+
+	public String getExceptionName() {
+		return exceptionName;
+	}
+
+	public void setExceptionName(String exceptionName) {
+		this.exceptionName = exceptionName;
+	}
+
+	public String getExceptionDescription() {
+		return exceptionDescription;
+	}
+
+	public void setExceptionDescription(String exceptionDescription) {
 		this.exceptionDescription = exceptionDescription;
 	}
     
