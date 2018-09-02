794fdcd5defa99d97dae44670e3c1f1c52738dba
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-02 23:23:46 +0530

    Update EmployeeQuery.java

--- EDA/Assignment4/dao/EmployeeQuery.java
+++ EDA/Assignment4/dao/EmployeeQuery.java
@@ -4,4 +4,15 @@ public class EmployeeQuery {
 
 	public static String addEmployeeQuery = "INSERT INTO employee (firstName, lastName, dateOfBirth, emailID, age, "
 			+ "contactNumber, password, companyId) values (?, ?, ?, ?, ?, ?, ?, ?)";
+	public static String getPasswordByEmailId = "SELECT password FROM employee WHERE emailID = ?";
+	
+	public static String getEmployeeDetailByEmailId = "SELECT employeeId, age, firstName, lastName, dateOfBirth, emailID, contactNumber, companyId "
+			                                        + " FROM employee WHERE emailID = ?";
+	public static String getCompanyNameByComanyId = "SELECT companyName FROM company WHERE companyId = ?";
+	
+	public static String getFriendListOfEmployeeId = "SELECT employeeFriendId FROM friends WHERE employeeId = ?";
+	
+	public static String getEmployeeById = "SELECT employeeId, age, firstName, lastName, dateOfBirth, emailID, contactNumber, companyId "
+                                         + " FROM employee WHERE employeeId = ?";
+	
 }
