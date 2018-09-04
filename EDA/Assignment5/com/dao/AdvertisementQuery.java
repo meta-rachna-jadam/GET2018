134608b51166c434f5b20242906f54cac85ade9a
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-04 18:54:20 +0530

    Update AdvertisementQuery.java

--- EDA/Assignment5/com/dao/AdvertisementQuery.java
+++ EDA/Assignment5/com/dao/AdvertisementQuery.java
@@ -4,5 +4,5 @@ public class AdvertisementQuery {
 
     public static String addAdvertisement = "INSERT INTO advertisement (advertisementTitle, advertisementDESC, categoryId) VALUES (?, ?, ?)";
     
-    public static String getAllAdvertisement = "SELECT advertisementTitle, advertisementDESC, categoryId from advertisement";
+    public static String getAllAdvertisement = "SELECT advertisementId, advertisementTitle, advertisementDESC, categoryId from advertisement";
 }
