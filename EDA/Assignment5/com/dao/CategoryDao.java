ab6f7e67786ec5537a0f4b739192ac8076e21655
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-04 18:54:43 +0530

    Update CategoryDao.java

--- EDA/Assignment5/com/dao/CategoryDao.java
+++ EDA/Assignment5/com/dao/CategoryDao.java
@@ -1,5 +1,35 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.constant.DBConstants;
+import com.model.Category;
+
 public class CategoryDao {
+	
+    public List<Category> getAllCategories() {
+    	List<Category> categoryList = new ArrayList<Category>();
+    	try (
+    	    Connection connectionInstance = JDBCConnection.getConnection(DBConstants.databaseName, 
+                DBConstants.userName, DBConstants.password);
+            PreparedStatement preparedStatementToAddAdvertisements = connectionInstance.prepareStatement(CategoryQuery.getAllCategory);) {
+            	
+            ResultSet categoryResultSet = preparedStatementToAddAdvertisements.executeQuery();
+            while (categoryResultSet.next()) {
+            	categoryList.add(new Category(categoryResultSet.getInt("categoryId"), categoryResultSet.getString("categoryName")));
+            }
+            return categoryList;
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            System.out.println("error "+sqlException.toString());
+            return null;
+    	}
+    }
+
 
 }
