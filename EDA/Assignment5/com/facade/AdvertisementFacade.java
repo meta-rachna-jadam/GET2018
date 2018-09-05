0efbcffa721a7883bb73111738a2904ee3e235b1
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 16:30:55 +0530

    Update AdvertisementFacade.java

--- EDA/Assignment5/com/facade/AdvertisementFacade.java
+++ EDA/Assignment5/com/facade/AdvertisementFacade.java
@@ -12,18 +12,47 @@ public class AdvertisementFacade {
         advertisementDao = new AdvertisementDao();
     }
     
+    /**
+	 * To add advertisement in database
+	 * @param advertisement
+	 * @return 1 if add successfully, 0 if add failed and -1 if error 
+	 */
     public int addAdvertisement(Advertisement advertisement)  {
         return advertisementDao.addAdvertisement(advertisement);
     }
+    
+    /**
+	 * To get all advertisements from database
+	 * @return list of advertisements
+	 */
     public List<Advertisement> getAllAdvertisements() {
     	return advertisementDao.getAllAdvertisements();
     }
+    
+    /**
+     * To get all advertisements from database by categoryId
+     * @param categoryId
+     * @return list of advertisements
+     */
     public List<Advertisement> getAllAdvertisementByCategoryId(int categoryId) {
     	return advertisementDao.getAllAdvertisementByCategoryId(categoryId);
     } 
+    
+    /**
+     * To delete advertisement from database by advertisementId
+     * @param advertisementId
+     * @return 1 if delete successfully, 0 if delete failed and -1 if error 
+     */
     public int deleteAdvertisementById(int advertisementId) {
     	return advertisementDao.deleteAdvertisementById(advertisementId);
     } 
+    
+    /**
+     * To update advertisementTitle in database
+     * @param advertisementTitle1 update to
+     * @param advertisementTitle2 initial value
+     * @return -1 if error, 0 if update failed and else update successfully
+     */
     public int updateAdvertisementByTitle(String advertisementTitle1, String advertisementTitle2) {
     	return advertisementDao.updateAdvertisementByTitle(advertisementTitle1, advertisementTitle2);
     } 
