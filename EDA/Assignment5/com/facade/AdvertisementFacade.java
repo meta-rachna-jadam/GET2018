6016f50b886471a381f3036a2214f347eccac39e
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 15:19:37 +0530

    Update AdvertisementFacade.java

--- EDA/Assignment5/com/facade/AdvertisementFacade.java
+++ EDA/Assignment5/com/facade/AdvertisementFacade.java
@@ -11,10 +11,20 @@ public class AdvertisementFacade {
     public AdvertisementFacade() {
         advertisementDao = new AdvertisementDao();
     }
+    
     public int addAdvertisement(Advertisement advertisement)  {
         return advertisementDao.addAdvertisement(advertisement);
     }
     public List<Advertisement> getAllAdvertisements() {
     	return advertisementDao.getAllAdvertisements();
     }
+    public List<Advertisement> getAllAdvertisementByCategoryId(int categoryId) {
+    	return advertisementDao.getAllAdvertisementByCategoryId(categoryId);
+    } 
+    public int deleteAdvertisementById(int advertisementId) {
+    	return advertisementDao.deleteAdvertisementById(advertisementId);
+    } 
+    public int updateAdvertisementByTitle(String advertisementTitle1, String advertisementTitle2) {
+    	return advertisementDao.updateAdvertisementByTitle(advertisementTitle1, advertisementTitle2);
+    } 
 }
