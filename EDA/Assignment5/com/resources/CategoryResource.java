15c0c7247429b70d9fb26d1299ee78f1538d2f42
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 15:20:49 +0530

    Update CategoryResource.java

--- EDA/Assignment5/com/resources/CategoryResource.java
+++ EDA/Assignment5/com/resources/CategoryResource.java
@@ -2,8 +2,12 @@ package com.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -26,4 +30,50 @@ public class CategoryResource {
 		Gson gson = new Gson();
 		return gson.toJson(categoryList);
 	}	
+	
+	@POST
+    @Path("/addCategory")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean addCategory(String categoryInput, @HeaderParam("Authorization") String authorizationString)
+    {
+        if(!"GET-2018".equals(authorizationString))
+        {
+            return false;
+        }
+        Gson g = new Gson();
+        Category category = g.fromJson(categoryInput, Category.class);
+        boolean result = (new CategoryConroller().addCategory(category) > 0);
+        return result;
+    }
+    
+    @DELETE
+    @Path("/deleteCategory")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean deleteCategory(String categoryID, @HeaderParam("Authorization") String authorizationString)
+    {
+        if(!"GET-2018".equals(authorizationString))
+        {
+            return false;
+        }
+        int id = Integer.parseInt(categoryID);
+        boolean result = (new CategoryConroller().deleteCategoryById(id) > 0);
+        return result;
+    }
+    
+    @PUT
+    @Path("/updateCategory")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean updateCategory(String categoryNames, @HeaderParam("Authorization") String authorizationString)
+    {
+        if(!"GET-2018".equals(authorizationString))
+        {
+            return false;
+        }
+        String[] categoryName = categoryNames.split(" to ");
+        boolean result = (new CategoryConroller().updateCategoryName(categoryName[1], categoryName[0]) > 0);
+        return result;
+    }
 }
