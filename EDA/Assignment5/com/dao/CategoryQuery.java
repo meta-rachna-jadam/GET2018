0bede87441079860aa3774f43e6e1cfa8317ee98
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 15:18:48 +0530

    Update CategoryQuery.java

--- EDA/Assignment5/com/dao/CategoryQuery.java
+++ EDA/Assignment5/com/dao/CategoryQuery.java
@@ -2,6 +2,8 @@ package com.dao;
 
 public class CategoryQuery {
 
-	public static String addCategory = "INSERT INTO category (categoryName) VALUES (?)";
-	public static String getAllCategory = "SELECT categoryId, categoryName FROM category";
+    public static final String addCategory = "INSERT INTO category (categoryName) VALUES (?)";
+    public static final String getAllCategory = "SELECT categoryId, categoryName FROM category";
+    public static final String deleteCategory = "DELETE FROM category WHERE categoryId = ?";
+    public static final String updateCategoryName = "UPDATE category SET categoryName = ? WHERE categoryName = ? ";
 }
