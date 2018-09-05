797a1404b31e0d1f90edb57014a997ddc402ff52
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 16:09:20 +0530

    Update DBConstants.java

--- EDA/Assignment5/com/constant/DBConstants.java
+++ EDA/Assignment5/com/constant/DBConstants.java
@@ -2,7 +2,7 @@ package com.constant;
 
 public class DBConstants {
 
-	public static final String userName = "root";
-	public static final String password = "r@jkum@ri";
-	public static final String databaseName = "restDemo";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "r@jkum@ri";
+	public static final String DATABASE_NAME = "restDemo";
 }
