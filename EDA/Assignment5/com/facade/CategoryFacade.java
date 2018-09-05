46c637cfacfe4a3676027c9c1c44c714dee30a13
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 16:58:26 +0530

    Update CategoryFacade.java

--- EDA/Assignment5/com/facade/CategoryFacade.java
+++ EDA/Assignment5/com/facade/CategoryFacade.java
@@ -1,31 +1,92 @@
-package com.facade;
+package com.resources;
 
 import java.util.List;
 
-import com.dao.CategoryDao;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.controller.AdvertisementController;
+import com.controller.CategoryConroller;
+import com.google.gson.Gson;
 import com.model.Category;
+import com.model.CustemException;
 
-public class CategoryFacade {
+@Path("category")
+public class CategoryResource {
+
+	private final String errorMessage = "Error: 401 Unauthorized Access";
+	private final String authorization = "GET-2018";
+	private int id;
+	private Gson gson = new Gson();
 	
-	CategoryDao categoryDao;
-    public CategoryFacade() {
-    	categoryDao = new CategoryDao();
+	@GET
+	@Path("getCategory")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAdvertisement(@HeaderParam("Authorization") String authorizationString) {
+		if (!authorization.equals(authorizationString)) {
+			return errorMessage;
+		}
+		List<Category> categoryList = new CategoryConroller().getAllCategories();
+		return gson.toJson(categoryList);
+	}	
+	
+	@POST
+    @Path("/addCategory")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean addCategory(String categoryInput, @HeaderParam("Authorization") String authorizationString)
+    {
+        if(!authorization.equals(authorizationString))
+        {
+            return false;
+        }
+        Category category = gson.fromJson(categoryInput, Category.class);
+        return  (new CategoryConroller().addCategory(category) > 0);
     }
     
-    public List<Category> getAllCategories() {
-    	return categoryDao.getAllCategories();
+    @DELETE
+    @Path("/deleteCategory")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean deleteCategory(String categoryID, @HeaderParam("Authorization") String authorizationString)
+    {
+        if(!authorization.equals(authorizationString))
+        {
+            return false;
+        }
+        id=-1;
+        try {
+            id = Integer.parseInt(categoryID);
+        }catch (NumberFormatException numberFormatException) {
+        	new CustemException("CategoryResource", numberFormatException.toString(), numberFormatException.getMessage());
+        	System.exit(1);
+        }
+        return (new CategoryConroller().deleteCategoryById(id) > 0);
     }
     
-    public int addCategory(Category category) {
-    	return categoryDao.addCategory(category);
+    @PUT
+    @Path("/updateCategory")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean updateCategory(String categoryNames, @HeaderParam("Authorization") String authorizationString)
+    {
+        if(!authorization.equals(authorizationString))
+        {
+            return false;
+        }
+        if (categoryNames.contains(" to ")){
+        	String[] advertisementTitle = categoryNames.split(" to ");
+        	return (new AdvertisementController().updateAdvertisementByTitle(advertisementTitle[1], advertisementTitle[0]) > 0);
+        } else {
+        	new CustemException("CategoryResource", "to is not present in given string", "to is not present in given string");
+        }
+        return false;
     }
-    
-    public int deleteCategoryById(int categoryId) {
-    	return categoryDao.deleteCategory(categoryId);
-    }
-    
-    public int updateCategoryName(String categoryName1, String categoryName2){
-    	return categoryDao.updateCategoryName(categoryName1, categoryName2);
-    }
-
 }
