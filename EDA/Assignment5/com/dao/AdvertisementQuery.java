d8f61c6dae01da28fd6314766e718e5bdb1b736c
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 15:17:35 +0530

    Update AdvertisementQuery.java

--- EDA/Assignment5/com/dao/AdvertisementQuery.java
+++ EDA/Assignment5/com/dao/AdvertisementQuery.java
@@ -2,7 +2,14 @@ package com.dao;
 
 public class AdvertisementQuery {
 
-    public static String addAdvertisement = "INSERT INTO advertisement (advertisementTitle, advertisementDESC, categoryId) VALUES (?, ?, ?)";
+    public static final String addAdvertisement = "INSERT INTO advertisement (advertisementTitle, advertisementDESC, categoryId) VALUES (?, ?, ?)";
     
-    public static String getAllAdvertisement = "SELECT advertisementId, advertisementTitle, advertisementDESC, categoryId from advertisement";
+    public static final String getAllAdvertisement = "SELECT advertisementId, advertisementTitle, advertisementDESC, categoryId from advertisement";
+    
+    public static final  String getAllAdvertisementByCategoryId = "SELECT advertisementId, advertisementTitle, advertisementDESC "
+    		                                                + "FROM advertisement WHERE categoryId = ?";
+    
+    public final static String DELETE_ADVERTISEMENT_BY_ID = "DELETE FROM advertisement WHERE advertisementId = ?";
+    
+    public final static String UPDATE_ADVERTISEMENT = "UPDATE advertisement SET advertisementTitle = ? WHERE advertisementTitle = ? ";
 }
