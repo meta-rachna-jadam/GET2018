d002b1738ac1b895d72e0f57cd0dc5536afd1d46
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 15:15:46 +0530

    Update AdvertisementController.java

--- EDA/Assignment5/com/controller/AdvertisementController.java
+++ EDA/Assignment5/com/controller/AdvertisementController.java
@@ -18,4 +18,14 @@ public class AdvertisementController {
     public List<Advertisement> getAllAdvertisements() {
     	return advertisementFacade.getAllAdvertisements();
     }
+    
+    public List<Advertisement> getAllAdvertisementByCategoryId(int categoryId) {
+    	return advertisementFacade.getAllAdvertisementByCategoryId(categoryId);
+    } 
+    public int deleteAdvertisementById(int advertisementId) {
+    	return advertisementFacade.deleteAdvertisementById(advertisementId);
+    } 
+    public int updateAdvertisementByTitle(String advertisementTitle1, String advertisementTitle2) {
+    	return advertisementFacade.updateAdvertisementByTitle(advertisementTitle1, advertisementTitle2);
+    } 
 }
