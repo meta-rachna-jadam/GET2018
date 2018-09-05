42615a8e2fbbf387ffcdcdf53ffc6a90ac3c5b8a
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 15:20:00 +0530

    Update CategoryFacade.java

--- EDA/Assignment5/com/facade/CategoryFacade.java
+++ EDA/Assignment5/com/facade/CategoryFacade.java
@@ -15,5 +15,17 @@ public class CategoryFacade {
     public List<Category> getAllCategories() {
     	return categoryDao.getAllCategories();
     }
+    
+    public int addCategory(Category category) {
+    	return categoryDao.addCategory(category);
+    }
+    
+    public int deleteCategoryById(int categoryId) {
+    	return categoryDao.deleteCategory(categoryId);
+    }
+    
+    public int updateCategoryName(String categoryName1, String categoryName2){
+    	return categoryDao.updateCategoryName(categoryName1, categoryName2);
+    }
 
 }
