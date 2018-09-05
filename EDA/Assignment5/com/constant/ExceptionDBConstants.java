f2aed1d6e022a1243f267a0bf178d051c40d26ac
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 17:02:12 +0530

    Add files via upload

---
+++ EDA/Assignment5/com/constant/ExceptionDBConstants.java
@@ -0,0 +1,9 @@
+package com.constant;
+
+public class ExceptionDBConstants {
+     
+	public static final String databaseName = "excetionDB";
+	public static final String successful = "Operation Perfom Successfully";
+	public static final String sqlError = "SQL Exception!! Please Enter valid details";
+	public static final String notSuccessful = "operation not perform";
+}
