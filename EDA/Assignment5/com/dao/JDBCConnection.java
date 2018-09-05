c0c218eee207cd25e2597f8021386dbec9cadf1f
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-05 17:01:24 +0530

    Update JDBCConnection.java

--- EDA/Assignment5/com/dao/JDBCConnection.java
+++ EDA/Assignment5/com/dao/JDBCConnection.java
@@ -4,27 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.model.CustemException;
+
 public class JDBCConnection {
  
-	public static Connection getConnection(String databaseName, String uername, String password)
-	  {
+	public static Connection getConnection(String databaseName, String uername, String password) {
 	    Connection connectionInstance = null;
-	    try
-	    {
+	    try {
 	      Class.forName("com.mysql.jdbc.Driver");
 	      String url = "jdbc:mysql://localhost:3306/"+ databaseName;
 	      connectionInstance = DriverManager.getConnection(url, uername , password);
-	    }
-	    catch (ClassNotFoundException classNotFoundException)
-	    {
-	      classNotFoundException.printStackTrace();
-	      System.exit(1);
-	    }
-	    catch (SQLException sqlException)
-	    {
-	      sqlException.printStackTrace();
+	    } catch (ClassNotFoundException classNotFoundException) {
+	    	new CustemException("JDBCConnection", classNotFoundException.toString(), classNotFoundException.getMessage());
+	        System.exit(1);
+	    } catch (SQLException sqlException) {
+	    	new CustemException("JDBCConnection", sqlException.toString(), sqlException.getMessage());
 	      System.exit(2);
-	    }
+	    } catch (Exception exception) {
+	    	new CustemException("JDBCConnection", exception.toString(), exception.getMessage());
+		    System.exit(2);
+		}
 	    return connectionInstance;
 	  }
 }
