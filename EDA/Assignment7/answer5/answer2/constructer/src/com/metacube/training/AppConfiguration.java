962cf78f9315c5883eb27960d4698f4aa22a95fa
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-06 14:00:32 +0530

    Update AppConfiguration.java

--- EDA/Assignment7/answer5/answer2/constructer/src/com/metacube/training/AppConfiguration.java
+++ EDA/Assignment7/answer5/answer2/constructer/src/com/metacube/training/AppConfiguration.java
@@ -3,6 +3,12 @@ package com.metacube.training;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * A configuration class which define all configuration required to create 
+ * object by BeanFactory or ApplicationContext  
+ * @author Rachna Jadam
+ *
+ */
 @Configuration
 public class AppConfiguration {
 	
