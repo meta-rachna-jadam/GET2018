827745f657a8ed4ccbf59fbfee691529ffee10eb
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-06 12:22:23 +0530

    Update TextEditor.java

--- EDA/Assignment7/answer2/byName/src/com/metacube/training/TextEditor.java
+++ EDA/Assignment7/answer2/byName/src/com/metacube/training/TextEditor.java
@@ -2,7 +2,7 @@ package com.metacube.training;
 
 public class TextEditor {
 
-    SpellChecker spellChecker;
+    private SpellChecker spellChecker;
 
 	public void setSpellChecker(SpellChecker spellChecker) {
 		this.spellChecker = spellChecker;
