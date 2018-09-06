f8b3c18c6f1ff69909c10a25191e3e2083e6b5a1
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-06 12:22:09 +0530

    Update SpellChecker.java

--- EDA/Assignment7/answer2/byName/src/com/metacube/training/SpellChecker.java
+++ EDA/Assignment7/answer2/byName/src/com/metacube/training/SpellChecker.java
@@ -2,8 +2,8 @@ package com.metacube.training;
 
 public class SpellChecker {
 	
-    public String status;
-    public boolean state;  
+    private String status;
+    private boolean state;  
     // true for On, false for Off
     
     public void setStatus(String status) {
@@ -23,5 +23,4 @@ public class SpellChecker {
     public boolean getState() {
         return state;
     }
-
 }
