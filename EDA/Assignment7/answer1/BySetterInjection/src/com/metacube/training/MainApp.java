bd4cffe923c3560867e03221a676563275181897
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-06 12:48:33 +0530

    Update MainApp.java

--- EDA/Assignment7/answer1/BySetterInjection/src/com/metacube/training/MainApp.java
+++ EDA/Assignment7/answer1/BySetterInjection/src/com/metacube/training/MainApp.java
@@ -8,11 +8,8 @@ public class MainApp {
     public static void main(String[] rugumentLis) {
     	
         ApplicationContext contex = new ClassPathXmlApplicationContext("beans.xml");
-        SpellChecker spellChecker = (SpellChecker) contex.getBean("spellChecker");
         TextEditor textEditor = (TextEditor) contex.getBean("textEditor");
         System.out.println("By TextEditer reference "+textEditor.getSpellChecker().getStatus());
         System.out.println("By TextEditer reference "+textEditor.getSpellChecker().getState());
-        System.out.println("By SpellChecker reference "+spellChecker.getStatus());
-        System.out.println("By SpellChecker reference "+spellChecker.getState());
     }
 }
