eba89b11c61a0a901eea1108a524fd1a9b2e6771
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-10 13:00:44 +0530

    Update EmployeeQuery.java

--- EDA/Assignment8/src/main/java/com/metacube/training/dao/query/EmployeeQuery.java
+++ EDA/Assignment8/src/main/java/com/metacube/training/dao/query/EmployeeQuery.java
@@ -20,4 +20,6 @@ public interface EmployeeQuery {
     String GET_EMPLOYEE_BY_ID = "SELECT emp_code, first_name, middle_name, dob, gender, primary_contact_no, "
                               + "secondary_contact_no, email_id, skype_id, profile_picture, enabled FROM employee "
     		                  + "WHERE emp_code = ?";
+    
+   String GET_EMPLOYEE_ID_BY_EMAIL_ID = "SELECT emp_code from employee WHERE email_id = ?";
 }
