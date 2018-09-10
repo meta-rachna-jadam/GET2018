4d338a1f8c0ff8a9810c5bdc4cb32f08ae32ef40
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-10 13:04:37 +0530

    Update JDBCConnection.java

--- EDA/Assignment8/src/main/java/com/metacube/training/dao/JDBCConnection.java
+++ EDA/Assignment8/src/main/java/com/metacube/training/dao/JDBCConnection.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.metacube.training.model.CustomException;
+
 public class JDBCConnection {
  
     public static Connection getConnection(String databaseName, String uername, String password) {
@@ -14,9 +16,11 @@ public class JDBCConnection {
             String url = "jdbc:mysql://localhost:3306/"+ databaseName;
             connectionInstance = DriverManager.getConnection(url, uername , password);
         } catch (ClassNotFoundException classNotFoundException) {
+        	new CustomException("JDBCConnection", classNotFoundException.toString(), classNotFoundException.getMessage());
             classNotFoundException.printStackTrace();
             System.exit(1);
         } catch (SQLException sqlException) {
+        	new CustomException("JDBCConnection", sqlException.toString(), sqlException.getMessage());
             sqlException.printStackTrace();
             System.exit(2);
         }
