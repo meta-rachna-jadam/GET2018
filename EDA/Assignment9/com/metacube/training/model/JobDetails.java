c6a05020c4b598abf49d7a12f6b92dd1af8c4484
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-12 13:06:02 +0530

    Update JobDetails.java

--- EDA/Assignment9/com/metacube/training/model/JobDetails.java
+++ EDA/Assignment9/com/metacube/training/model/JobDetails.java
@@ -2,10 +2,13 @@ package com.metacube.training.model;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class JobDetails {
 
     private int empCode;
     private int Id;
+    @DateTimeFormat(pattern= "yyyy-MM-dd")
     private Date dateOfJoining;
     private int totalExperience;
     private int jobCode;
@@ -13,62 +16,52 @@ public class JobDetails {
     private int teamLeaderId;
     private int currentProjectId;
     
-	public JobDetails(int empCode, int id, Date dateOfJoining,
-			int totalExperience, int jobCode, int reportingManagerId,
-			int teamLeaderId, int currentProjectId) {
-		super();
-		this.empCode = empCode;
-		Id = id;
-		this.dateOfJoining = dateOfJoining;
-		this.totalExperience = totalExperience;
-		this.jobCode = jobCode;
-		this.reportingManagerId = reportingManagerId;
-		this.teamLeaderId = teamLeaderId;
-		this.currentProjectId = currentProjectId;
-	}
-
-	public JobDetails(int empCode, Date dateOfJoining, int totalExperience,
-			int jobCode, int reportingManagerId, int teamLeaderId,
-			int currentProjectId) {
-		super();
-		this.empCode = empCode;
-		this.dateOfJoining = dateOfJoining;
-		this.totalExperience = totalExperience;
-		this.jobCode = jobCode;
-		this.reportingManagerId = reportingManagerId;
-		this.teamLeaderId = teamLeaderId;
-		this.currentProjectId = currentProjectId;
-	}
-
 	public int getEmpCode() {
 		return empCode;
 	}
-
 	public int getId() {
 		return Id;
 	}
-
 	public Date getDateOfJoining() {
 		return dateOfJoining;
 	}
-
 	public int getTotalExperience() {
 		return totalExperience;
 	}
-
 	public int getJobCode() {
 		return jobCode;
 	}
-
 	public int getReportingManagerId() {
 		return reportingManagerId;
 	}
-
 	public int getTeamLeaderId() {
 		return teamLeaderId;
 	}
-
 	public int getCurrentProjectId() {
 		return currentProjectId;
+	}
+	public void setEmpCode(int empCode) {
+		this.empCode = empCode;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public void setDateOfJoining(Date dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
+	}
+	public void setTotalExperience(int totalExperience) {
+		this.totalExperience = totalExperience;
+	}
+	public void setJobCode(int jobCode) {
+		this.jobCode = jobCode;
+	}
+	public void setReportingManagerId(int reportingManagerId) {
+		this.reportingManagerId = reportingManagerId;
+	}
+	public void setTeamLeaderId(int teamLeaderId) {
+		this.teamLeaderId = teamLeaderId;
+	}
+	public void setCurrentProjectId(int currentProjectId) {
+		this.currentProjectId = currentProjectId;
 	}
 }
