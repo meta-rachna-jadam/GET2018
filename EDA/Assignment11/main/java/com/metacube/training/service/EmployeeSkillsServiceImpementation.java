6d4c16ab429c4bd7c27eaab038ad2ba267130199
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-15 18:21:23 +0530

    Update EmployeeSkillsServiceImpementation.java

--- EDA/Assignment11/main/java/com/metacube/training/service/EmployeeSkillsServiceImpementation.java
+++ EDA/Assignment11/main/java/com/metacube/training/service/EmployeeSkillsServiceImpementation.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.metacube.training.dao.EmployeeSkillsDAO;
+import com.metacube.training.model.Employee;
 import com.metacube.training.model.EmployeeSkills;
 
 @Service
