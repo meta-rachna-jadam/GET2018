07f0865eb69b7c4c1a2a5702a7a8bebc3e5cf7d6
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-15 18:25:37 +0530

    Update JobDetailsRepository.java

--- EDA/Assignment12/main/java/com/metacube/training/repository/JobDetailsRepository.java
+++ EDA/Assignment12/main/java/com/metacube/training/repository/JobDetailsRepository.java
@@ -1,10 +1,17 @@
 package com.metacube.training.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.metacube.training.model.Employee;
 import com.metacube.training.model.JobDetails;
 
 
 public interface JobDetailsRepository<P> extends JpaRepository<JobDetails, Integer> {
 
+	@Query("select e from JobDetails jd inner join jd.employee e inner join jd.project p where "
+			+ "p.description = :description")
+	public List<Employee> searchEmployeeByProject(@Param("description") String description); 
 }
