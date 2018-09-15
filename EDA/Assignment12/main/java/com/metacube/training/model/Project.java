557b93dffc71065ff1e6868374305eb0b8a93f33
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-09-15 18:25:12 +0530

    Update Project.java

--- EDA/Assignment12/main/java/com/metacube/training/model/Project.java
+++ EDA/Assignment12/main/java/com/metacube/training/model/Project.java
@@ -1,90 +1,121 @@
 package com.metacube.training.model;
 
 import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 /**
- * Entity class map with project table of database
+ * Entity class map with jobDetails table of database
  * @author Rachna Jadam
  *
  */
 
 @Entity
-@Table(name = "project")
-public class Project {
+@Table(name = "jobDetails")
+public class JobDetails {
 
-	@Id
-	@Column(name = "project_id")
+	
+	@Column(name = "emp_code")
+	private int empCode;
+    
+    @Id
+	@Column(name = "job_detail_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
-	@Column(name = "project_logo")
-	private String logo;
-	
-	@Column(name = "description")
-	private String description;
-	
-	@Column(name = "start_date")
+    private int id;
+    
+    @Column(name = "date_of_joining")
 	@DateTimeFormat(pattern= "yyyy-MM-dd")
 	@Temporal(TemporalType.DATE)
-	private Date startDate;
-	
-	@Column(name = "end_date")
-	@DateTimeFormat(pattern= "yyyy-MM-dd")
-	@Temporal(TemporalType.DATE)
-	private Date endDate;
-	
-	@Override
+    private Date dateOfJoining;
+    
+    @Column(name = "total_exp")
+    private int totalExperience;
+    
+    @Column(name = "job_code")
+    private int jobCode;
+    
+    @Column(name = "reporting_mgr")
+    private int reportingManagerId;
+    
+    @Column(name = "team_lead")
+    private int teamLeaderId;
+    
+    @Column(name = "curr_proj_id")
+    private int currentProjectId;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "emp_code", insertable = false, updatable = false)
+    private Employee employee;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "curr_proj_id", insertable = false, updatable = false)
+    private Project project;
+    
+    @Override
 	public String toString() {
-		return "Project [id=" + id + ", logo=" + logo + ", description=" + description 
-				+ ", start_date=" + startDate + ", endDate=" + endDate + "]";
+		return "Job Detail [id=" + id + ", dateOfJoining=" + dateOfJoining + ", totalExperience="
+				+ totalExperience + ", teamLeaderId=" + teamLeaderId + ", reportingManagerId="
+				+ reportingManagerId +", currentProjectId"+ currentProjectId + "]";
 	}
-
+    
+	public int getEmpCode() {
+		return empCode;
+	}
 	public int getId() {
 		return id;
 	}
-
+	public Date getDateOfJoining() {
+		return dateOfJoining;
+	}
+	public int getTotalExperience() {
+		return totalExperience;
+	}
+	public int getJobCode() {
+		return jobCode;
+	}
+	public int getReportingManagerId() {
+		return reportingManagerId;
+	}
+	public int getTeamLeaderId() {
+		return teamLeaderId;
+	}
+	public int getCurrentProjectId() {
+		return currentProjectId;
+	}
+	public void setEmpCode(int empCode) {
+		this.empCode = empCode;
+	}
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getLogo() {
-		return logo;
+	public void setDateOfJoining(Date dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
 	}
-
-	public void setLogo(String logo) {
-		this.logo = logo;
+	public void setTotalExperience(int totalExperience) {
+		this.totalExperience = totalExperience;
 	}
-
-	public String getDescription() {
-		return description;
+	public void setJobCode(int jobCode) {
+		this.jobCode = jobCode;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	public void setReportingManagerId(int reportingManagerId) {
+		this.reportingManagerId = reportingManagerId;
 	}
-
-	public Date getStartDate() {
-		return startDate;
+	public void setTeamLeaderId(int teamLeaderId) {
+		this.teamLeaderId = teamLeaderId;
 	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
+	public void setCurrentProjectId(int currentProjectId) {
+		this.currentProjectId = currentProjectId;
 	}
 }
