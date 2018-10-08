e8cab8dd6f47047cc918af73274c0844600a2d13
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-10-08 12:17:53 +0530

    Add files via upload

---
+++ Assignment8/Repeatify.js
@@ -0,0 +1,14 @@
+// Function to return a string repeated 'count' number of times
+String.prototype.repeatify = function(count)
+{
+   var outputString = "";
+   while(count > 0)
+   {
+       outputString += this;
+       count--;
+   }
+   return outputString;
+};
+
+//funtion calling
+console.log("hello".repeatify(3));
