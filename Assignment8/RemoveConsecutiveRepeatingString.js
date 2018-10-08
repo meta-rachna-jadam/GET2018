c10b0ceb0673414591f8a2c8f5b66751142e1883
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-10-08 10:56:19 +0530

    Update RemoveConsecutiveRepeatingString.js

--- Assignment8/RemoveConsecutiveRepeatingString.js
+++ Assignment8/RemoveConsecutiveRepeatingString.js
@@ -1,36 +1,16 @@
 function removeConsecutiveRepeatingString(inputString) {
-    //debugger;
-    var isCharAppearFirstTime = true,
-        resultString = inputString,
-        isCharAppearFirstTime = true,
-        repeatationStartIndex = -1;
-    for (var index = 0; index < resultString.length-1; index++) {
-        console.log("Start");
-        console.log(index);
-        console.log("repeatationStartIndex = "+repeatationStartIndex);
-        if (inputString.charAt(index) == inputString.charAt(index + 1)){
-            if (isCharAppearFirstTime) {
-                repeatationStartIndex = index;
-            }
-            isCharAppearFirstTime = false;
-        } else if (inputString.charAt(index) != inputString.charAt(index + 1)) {
-            isCharAppearFirstTime = true;
-            
-            if (repeatationStartIndex != -1) {
-                console.log("test = "+resultString.substring(repeatationStartIndex,index+1));
-                resultString = resultString.replace(resultString.substring(repeatationStartIndex,index+1),"");
-                console.log("rs = "+resultString);
-                index = -1;
-                repeatationStartIndex = -1;
-            }
-            
-            
-        }
-        console.log("End");
-        
-    }
-    console.log("Result = "+resultString);
-    return resultString;
+   var count=0;
+	for (var index = 0; index < inputString.length-1; index++) {
+		if (inputString[index]===inputString[index + 1]) {
+			count++;
+		} else {
+			if (count>0) {
+				inputString = inputString.substring(0,index-count).concat(inputString.substring(index+1,inputString.length));
+				index=0;
+			}
+			count=0;
+		}
+	}
+	console.log(inputString);
+    return inputString;
 }
-
-removeConsecutiveRepeatingString("eeabcddcbfgf");
