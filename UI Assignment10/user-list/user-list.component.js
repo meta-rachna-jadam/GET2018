bae52eaabbc4e3d37949e078d779124017a61c72
meta-rachna-jadam <41181751+meta-rachna-jadam@users.noreply.github.com>
2018-10-11 16:18:38 +0530

    Update user-list.component.js

--- UI Assignment10/user-list/user-list.component.js
+++ UI Assignment10/user-list/user-list.component.js
@@ -1,13 +1,57 @@
-// Register `userList` component, along with its associated controller and template
-angular.
-  module('userList').
-  component('userList', {
-    templateUrl:  'user-list/user-list.template.html',
-    controller: ['$http', function UserListController($http) {
-      var self = this;
+/* Register `userList` component, along with its associated controller and template
 
-      $http.get('users/users.json').then(function(response) {
-        self.users = response.data;
-      });
+    The component part will executes all the functionalities of add, 
+    edit and displaying user data 
+*/
+
+angular.
+    module('userList').
+    component('userList', {
+        templateUrl: 'user-list/user-list.template.html',
+        controller: ['$http', '$scope', function UserListController($http, $scope) {
+            var self = this;
+            function createUserRef() {
+                self.user = {
+                picture: "images//user.jpg",
+                name: null,
+                phone: null,
+                address: null
+            }   
+        }
+
+        createUserRef();
+        $scope.createUser = function() {
+        $scope.header = "Add";
+            createUserRef();
+        }
+
+        $scope.editUser = function(userIndex) {
+            $scope.header = "Edit";
+            if(userIndex != null) {
+                self.user.name = self.users[userIndex].name;
+                self.user.phone = self.users[userIndex].phone;
+                self.user.address = self.users[userIndex].address;
+                self.user.userIndex = userIndex;
+            } 
+        }
+
+        $http.get('users/users.json').then(function(response) {
+            self.users = response.data;
+        });
+
+        $scope.addUser = function() {
+            if (self.user.userIndex != null){
+                self.users[self.user.userIndex] = self.user;
+            } else if (self.user.name != null && self.user.phone != null && self.user.address != null){
+                self.users.push(self.user);
+            } else{
+                alert("Please enter valid details");
+            }
+            createUserRef(); 
+        }
+
+        $scope.searchUser = function() {
+            self.query = document.getElementById("search-input").value;
+        }
     }]
-  });
+});
